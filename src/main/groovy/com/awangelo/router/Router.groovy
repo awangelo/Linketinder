1f2b3e3d7b41b736244c45fb3af4a8d9d7dceac8
awangelo <anggg.gaio@gmail.com>
2026-02-12 21:44:21 -0300

    feat: Add new routes to router

--- src/main/groovy/com/awangelo/router/Router.groovy
+++ src/main/groovy/com/awangelo/router/Router.groovy
@@ -8,12 +8,16 @@ final class Router {
     static getRouter(CandidatoController candidatoController,
                      EmpresaController empresaController,
                      VagaController vagaController) {
+
         new RoutingHandler()
                 .get("/candidatos", candidatoController.&listarTodos)
+                .post("/candidatos", candidatoController.&criar)
                 .get("/candidatos/{id}", candidatoController.&buscarPorId)
                 .get("/empresas", empresaController.&listarTodos)
+                .post("/empresas", empresaController.&criar)
                 .get("/empresas/{id}", empresaController.&buscarPorId)
                 .get("/vagas", vagaController.&listarTodos)
+                .post("/vagas", vagaController.&criar)
                 .get("/vagas/{id}", vagaController.&buscarPorId)
     }
 }
